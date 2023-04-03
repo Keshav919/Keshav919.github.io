75bd4578a1ac6dde7006c11717ffbd4cacdd9840
Keshav919 <krungta57@gmail.com>
2023-04-02 21:56:51 -0700

    Proper toggling between menu and launcher

--- scripts/hamburgerToggle.js
+++ scripts/hamburgerToggle.js
@@ -1,14 +1,26 @@
 function toggleHamburger() {
     var hamburger = document.getElementById("hamburger");
+    var menu = document.getElementById("menu");
+    var launcher = document.getElementById("launcher");
+    var timeline = document.getElementsByClassName("timeline")[0];
 
     var dir = hamburger.style.transform == "rotate(90deg)" ? -1 : 1;
 
     if(dir == 1){
+
+        timeline.style.display = "none";
+        launcher.style.transition = "transform 0.5s ease";
+        launcher.style.transform = "rotate(0deg)";
+
         hamburger.style.transition = "transform 0.5s ease";
         hamburger.style.transform = "rotate(90deg)";
+        setTimeout(function() {
+            menu.style.display = "block";
+        }, 500);
     }
     else{
         // Remove the timeline before the animation starts
+        menu.style.display = "none";
         hamburger.style.transition = "transform 0.5s ease";
         hamburger.style.transform = "rotate(0deg)";
     }
--- scripts/launcherToggle.js
+++ scripts/launcherToggle.js
@@ -1,15 +1,24 @@
 function toggleLauncher() {
     var launcher = document.getElementById("launcher");
     var timeline = document.getElementsByClassName("timeline")[0];
+    var hamburger = document.getElementById("hamburger");
+    var menu = document.getElementById("menu");
 
     var dir = launcher.style.transform == "rotate(90deg)" ? -1 : 1;
 
     if(dir == 1){
+        
+        menu.style.display = "none";
+        hamburger.style.transition = "transform 0.5s ease";
+        hamburger.style.transform = "rotate(0deg)";
+
         launcher.style.transition = "transform 0.5s ease";
         launcher.style.transform = "rotate(90deg)";
+
         // Add the timeline after the animation ends
         setTimeout(function() {
             timeline.style.display = "block";
+            menu.style.display = "none";
         }, 500);
     }
     else{
