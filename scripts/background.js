a70317444f5897e7e1eef7fbbed7489be90cdcbb
Keshav919 <krungta57@gmail.com>
2026-03-20 00:29:18 -0700

    try to salvage positioning

--- scripts/background.js
+++ scripts/background.js
@@ -4,13 +4,9 @@
         "../images/backgrounds/VP_Events.png",
         "../images/backgrounds/mn.png",
     ];
-
-    // Reuse the same image for the whole session, pick a new one on fresh load
-    let chosen = sessionStorage.getItem("landingBg");
-    if (!chosen) {
-        chosen = images[Math.floor(Math.random() * images.length)];
-        sessionStorage.setItem("landingBg", chosen);
-    }
+    
+    chosen = images[Math.floor(Math.random() * images.length)];
+    sessionStorage.setItem("landingBg", chosen);
 
     document.documentElement.style.setProperty("--landing-bg", `url(${chosen})`);
 })();
